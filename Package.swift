877085d6481683ab828b0a700ef3a286b49bab9e
Sami Moustachir <sami@xendit.co>
2022-09-06 10:54:18 +0200

    update package with dep target

--- Package.swift
+++ Package.swift
@@ -4,6 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "Xenissuing",
+    // Set minimum deployment target to macOS 10.12.
+    platforms: [
+        .macOS(.v10_12),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,20 +18,20 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", .exact("1.4.0")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Xenissuing",
-            dependencies: [],
-            exclude: ["README.md"]
+            dependencies: ["CryptoSwift"],
+            exclude: []
         ),
         .testTarget(
             name: "XenissuingTests",
             dependencies: ["Xenissuing"],
-            exclude: ["README.md"]
+            exclude: []
         ),
     ]
 )
