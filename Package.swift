3a91eddaf32861b19b6590bbbf2015856c9aea3b
Sami Moustachir <moustachir.sami@gmail.com>
2022-09-07 14:29:37 +0200

    fix: bump macos require

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "Xenissuing",
     // Set minimum deployment target to macOS 10.12.
     platforms: [
-        .macOS(.v10_12),
+        .macOS(.v10_15),
         .iOS(.v14)
     ],
     products: [
